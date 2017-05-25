04c213d46fcad2bfb95871f9c82141e8b0c82186
Unknown <davinsones1@gmail.com>
2017-05-25 15:18:15 -0700

    Constants Fully Formatted

--- src/Main_Window.java
+++ src/Main_Window.java
@@ -90,7 +90,7 @@ public class Main_Window extends JPanel {
 			if(whichMenu.equals("home")) {
 				frame.setSize(screenX,screenY);
 			} else {
-				frame.setSize(950, 750);
+				frame.setSize(1300, 750);
 			}
 			Thread.sleep(10);
 		}
